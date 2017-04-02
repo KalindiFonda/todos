90ae4a8430b530a55d538ecd307f82c287fecc7c
Kalindi Fonda <kalindi.fonda@gmail.com>
2017-04-01 18:32:07 -0700

    add todo count chart

--- compiler.py
+++ compiler.py
@@ -19,63 +19,85 @@ class Todo_day(ndb.Model):
     new = ndb.IntegerProperty()
     done = ndb.IntegerProperty()
     retired = ndb.IntegerProperty()
-    tot_day = ndb.IntegerProperty()
+    tot = ndb.IntegerProperty()
+    tot_on_day = ndb.IntegerProperty()
 
     todos_date = ndb.DateProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
 
 
+
+
+def get_tot():
+    entries = Todo_day.query().order(Todo_day.date).fetch()
+    print entries
+
+    total, new, done, retired = 0, 0, 0, 0
+
+    for entry in entries:
+        total += entry.tot
+        if entry.todos_date == datetime.date.today():
+            new += entry.new
+            done += entry.done
+            retired += entry.retired
+        entry.tot_on_day = total
+
+    template_values = {
+        "today" : time.strftime("%Y-%m-%d"),
+        "total" : total,
+        "new" : new,
+        "done" : done,
+        "retired" : retired,
+        "entries" : entries
+    }
+    return template_values
+
+def get_template_values(template):
+
+    get_values_function_mapper = {
+        "todocounter" : get_tot,
+        "chart": get_tot,
+    }
+
+    if template in get_values_function_mapper:
+        return get_values_function_mapper[template]()
+
+    return {}
+
+
 class Page(webapp2.RequestHandler):
 
-    def get(self, reg_input=""):
-        page_dictionary = {"": "index.html"}
-        html_template = page_dictionary[reg_input]
+    def get(self, reg_input="index"):
 
-        entries = Todo_day.query().order(-Todo_day.date).fetch()
-
-        total, new, done, retired = 0, 0, 0, 0
-
-        for entry in entries:
-            total += entry.tot_day
-            if entry.todos_date == datetime.date.today():
-                new = entry.new
-                done = entry.done
-                retired = entry.retired
-
-        template_values = {
-            "today" : time.strftime("%Y-%m-%d"),
-            "total" : total,
-            "new" : new,
-            "done" : done,
-            "retired" : retired
-        }
+        template_values = get_template_values(reg_input)
+        html_template = reg_input + ".html"
 
         template = JINJA_ENVIRONMENT.get_template(html_template)
         self.response.write(template.render(template_values))
 
 
-    def post(self):
+    def post(self, reg_input="index"):
 
         todo_new = int(self.request.get('new'))
         todo_done = int(self.request.get('done'))
         todo_retired = int(self.request.get('retired'))
-        tot_day = todo_new - todo_done - todo_retired
         todo_date = datetime.datetime.strptime(
                         self.request.get('date'), "%Y-%m-%d"
                     )
+        tot_day = todo_new - todo_done - todo_retired
 
         todo_day = Todo_day(
             new = todo_new,
             done = todo_done,
             retired = todo_retired,
-            tot_day = tot_day,
+            tot = tot_day,
             todos_date = todo_date
         )
 
         todo_day.put()
         time.sleep(.1)
 
-        self.redirect('/')
+        self.redirect('/' + reg_input)
 
 
-app = webapp2.WSGIApplication([(r'/', Page)], debug = True)
+app = webapp2.WSGIApplication([(r'/', Page), ('/(\w+)', Page)], debug = True)
